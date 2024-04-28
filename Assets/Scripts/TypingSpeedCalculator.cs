619915c4771e3d350438e70b4bf35dfaf144a4f4
cakeneka <nekaelia@protonmail.com>
2024-04-28 21:41:26 +0200

    UT-8 | refactor(cpm): simplify average calculation

--- Assets/Scripts/TypingSpeedCalculator.cs
+++ Assets/Scripts/TypingSpeedCalculator.cs
@@ -1,48 +1,32 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 public class TypingSpeedCalculator {
 
     private List<WordCPM> typedWords = new List<WordCPM>();
-    private float runningAverageCPM = 0f;
-    private float last10RunningAverageCPM = 0f;
     public float AverageCPM {
         get {
-            return runningAverageCPM;
+            return typedWords.Count() > 0 ?
+                typedWords.Select(c => c.cpm).Average()
+                :
+                0f;
         }
     }
+
     public float AverageCPMLast10 {
         get {
-            return last10RunningAverageCPM;
+            // return last10RunningAverageCPM;
+            return typedWords.Count() > 0 ?
+                // typedWords.Take(10).Select(c => c.cpm).Average()
+                typedWords.Skip(Math.Max(0, typedWords.Count() - 10)).Select(c => c.cpm).Average()
+                :
+                0f;
         }
     }
-
 
     public void AddWordCPM(WordCPM wordSpeed) {
-        AddToRunningAverage(wordSpeed.cpm);
-        AddToLast10RunningAverage(wordSpeed.cpm);
         typedWords.Add(wordSpeed);
-    }
-
-    private void AddToRunningAverage(float cpm) {
-        if (typedWords.Count == 0) {
-            runningAverageCPM = cpm;
-        } else {
-            // It calculates the new avg based on the previous avg, the new value and the amount of values.
-            float newSum = typedWords.Count * runningAverageCPM + cpm;
-            runningAverageCPM = newSum / (typedWords.Count + 1);
-        }
-    }
-
-    private void AddToLast10RunningAverage(float cpm) {
-        if (typedWords.Count == 0) {
-            last10RunningAverageCPM = cpm;
-        } else if (typedWords.Count < 10) {
-            float newSum = typedWords.Count * last10RunningAverageCPM + cpm;
-            last10RunningAverageCPM = newSum / (typedWords.Count + 1);
-        } else {
-            float avgToRemove = typedWords[typedWords.Count - 9].cpm;
-            float newSum = (10 * last10RunningAverageCPM) - avgToRemove + cpm;
-            last10RunningAverageCPM = newSum / 10;
-        }
     }
 }
